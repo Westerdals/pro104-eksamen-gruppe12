1ab4d794332046c512f0d288ac5e90ff58ec6346
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2020-05-15 21:14:41 +0200

    prøvd en ny funksjon for å få til overskrift på en en task og description

--- JS/ToDo.js
+++ JS/ToDo.js
@@ -1,43 +1,84 @@
-function addTodoItem() {
-  var todoItem = $("#new-todo-item").val();
-  $("#todo-list").append(
-    "<li><input type='checkbox'" +
-      " name='todo-item-done'" +
-      " class='todo-item-done'" +
-      " value='" +
-      todoItem +
-      "' /> " +
-      todoItem +
-      " <button class='todo-item-delete'>" +
-      "Delete</button></li>"
-  );
+// function addTodoTask() {
+//   var todoItem = $("#new-todo-item").val();
+//   $("#todo-list").append(
+//     "<li><input type='checkbox'" +
+//       " name='todo-item-done'" +
+//       " class='todo-item-done'" +
+//       " value='" +
+//       todoItem +
+//       "' /> " +
+//       todoItem +
+//       " <button class='todo-item-delete'>" +
+//       "Delete</button></li>"
 
-  $("#new-todo-item").val("");
+//   );
+
+//   $("#new-todo-item").val("");
+// }
+
+// function deleteTodoTask(e, item) { /* Delete function with a fade using jquery */
+//   e.preventDefault();
+//   $(item)
+//     .parent()
+//     .fadeOut("slow", function () {
+//       $(item).parent().remove();
+//     });
+// }
+
+// function completeTodoItem() { /* Completed function where the tasks who are finished is line-through */
+//   $(this).parent().toggleClass("strike");
+// }
+
+// $(function () {
+//   $("#add-todo-item").on("click", function (e) {
+//     e.preventDefault();
+//     addTodoTask();
+//   });
+
+//   $("#todo-list").on("click", ".todo-item-delete", function (e) {
+//     var item = this;
+//     deleteTodoTask(e, item);
+//   });
+
+//   $(document).on("click", ".todo-item-done", completeTodoItem);
+// });
+
+
+// localStorage.setItem('Tasks',)
+
+function saveTask(e){
+
+var taskName = document.getElementById("task-name").nodeValue;
+var taskDescription = document.getElementById("task-description").nodeValue;
+
+var tasks = {
+
+  name : taskName,
+  description:taskDescription
 }
 
-function deleteTodoItem(e, item) { /* Delete function with a fade using jquery */
-  e.preventDefault();
-  $(item)
-    .parent()
-    .fadeOut("slow", function () {
-      $(item).parent().remove();
-    });
+if(localStorage.getItem('tasks')==null){
+var task = []; task.push(tasks);
+localStorage.setItem('task', JSON.stringify(task));
+
+}else{
+  var myTask = localStorage.getItem('task', JSON.stringify(task)); myTask.push(task);
+  localStorage.setItem('task', JSON.stringify(myTask));
 }
 
-function completeTodoItem() { /* Completed function where the tasks who are finished is line-through */
-  $(this).parent().toggleClass("strike");
 }
 
-$(function () {
-  $("#add-todo-item").on("click", function (e) {
-    e.preventDefault();
-    addTodoItem();
-  });
+function getTask(){
+  var task = JSON.parse(localStorage.getItem('task'));
 
-  $("#todo-list").on("click", ".todo-item-delete", function (e) {
-    var item = this;
-    deleteTodoItem(e, item);
-  });
+var taskResult = document.getElementById('taskResult');
+taskResult.innerHTML = "";
 
-  $(document).on("click", ".todo-item-done", completeTodoItem);
-});
+for(var i = 0; i < task.length; i++){
+  var name = task[i].name;
+  var description = task[i].description;
+
+  taskResult.innerHTML += '<div class="well><h3>'+ name +' '+description+' </h3>'
+  '</div>'
+}
+}
