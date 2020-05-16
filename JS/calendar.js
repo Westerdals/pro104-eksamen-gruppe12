b636585cf2ebfff05766238742f5aad044f9b25c
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2020-05-16 19:35:18 +0200

    har fikset på to-do-listen, lar shaheb drive med css delen

--- JS/calendar.js
+++ JS/calendar.js
@@ -1,11 +1,17 @@
-const calendar = document.querySelector("#calendar");
+var calendar = document.getElementById("cd");
+console.log(cd);
 
-for (let day = 1; day <= 31; day++){
-    console.log(day);
-
-    calendar.insertAdjacentHTML("beforeend", 
-    `<div class="day">${day}</div>`);
-}
+// for(var day = 1; day <  31; day++){
+// console.log(day);
 
 
 
+ calendar.insertAdjacentElement("beforeend", '<div class=".day">${day}</div>');
+
+// }
+
+// for (let day = 1; day <= 31; day++){
+//     console.log(day);
+
+//     calendar.insertAdjacentHTML("beforeend", 
+//     '<div class=".day">${day}</div>');
