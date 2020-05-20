e0f05c728d420447c9e7091246e91152700c2042
Harald Sigvartsen <56087871+HaraldSigvartsen@users.noreply.github.com>
2020-05-20 12:55:12 +0200

    prøvd mye forskjellig med å få til at alle funksjoner kjører før newSite(), men får det ikke til

--- JS/index.js
+++ JS/index.js
@@ -1,99 +1,90 @@
-/* Getting variables from HTML */
-let form = document.getElementById('form');
+const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-const btn = document.getElementById('submitBtn');
-btn.onclick = registerWithUs;
-const Persons = [];
+nextPage = true;
 
-
-// Show input error message 
-function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-}
-
-// Show input success outline
-function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success'
-}
-
-// Checks if email is valid 
-function checkEmail(input) {      //Checks the email if its vanlig
-    const char = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (char.test(input.value.trim())) {
-        showSuccess(input);
-    } else {
-        showError(input, 'Email is not valid');
-    }
-}
-
-
-// Check required fields
-function checkRequired(inputArr) {
-    inputArr.forEach(function (input) {
-        if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`);
-        } else {
-            showSuccess(input);
-        }
-    });
-}
-
-// Check input length
-function checkLength(input, min, max) {
-    if (input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be atleast ${min} characters`);
-    } else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
-    } else {
-        showSuccess(input);
-    }
-}
-
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-    if (input1.value !== input2.value) {
-        showError(input2, 'Password does not mach');
-    }
-}
-
-
-// Get fieldname and make the first letter in the word uppercase
-function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-
-// Event listeners
-if(form)
-form.addEventListener('submit',function (e) {
-    e.preventDefault();
-
-/* Here we run the functions */
-    checkRequired([username, email, password, password2]);
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
-    checkEmail(email);
-    checkPasswordsMatch(password, password2);
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+	
+//local storage object
+var lsPerson = {
+    username: username.value,
+    email: email.value,
+    password: password.value
+};
+localStorage.setItem('PersonData', JSON.stringify(lsPerson));
+    
 });
 
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+        nextPage = false;
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+        nextPage = false;
+	} else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+        nextPage = false;
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+        nextPage = false;
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+        setErrorFor(password2, 'Password2 cannot be blank');
+        nextPage = false;
+	} else if(passwordValue !== password2Value) {
+        setErrorFor(password2, 'Passwords does not match');
+        nextPage = false;
+	} else{
+		setSuccessFor(password2);
+    }
+
+	if(nextPage === false){
+		console.log(newSite);
+	}else if(nextPage === true){
+		newSite(newpage);
+	}
+}
 
 
-/*Local Storage */
-function registerWithUs(){
-let lsPerson = {
-    username: username.value,
-    email:  email.value,
-    password:  password.value
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
 
-};
-   
-localStorage.setItem('PersonData',JSON.stringify(lsPerson));
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+function newSite(){
+   const newpage = window.location.href="/HTML/index.html";
 }
 
